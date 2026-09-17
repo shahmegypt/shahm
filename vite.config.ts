521b6dd3d94d7295e4c177f21ec0f87d9c66fa2e
shahmegypt <binochocos@gmail.com>
2026-09-17 14:14:52 +0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -26,19 +26,19 @@ export default defineConfig({
         dir: 'rtl',
         icons: [
           {
-            src: 'icon-192.png',
+            src: 'pwa-192x192.png',
             sizes: '192x192',
             type: 'image/png',
             purpose: 'any'
           },
           {
-            src: 'icon-512.png',
+            src: 'pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any'
           },
           {
-            src: 'icon-512.png',
+            src: 'pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'maskable'
